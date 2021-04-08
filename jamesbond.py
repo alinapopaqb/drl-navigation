9f5c858b76e7d1fc8c401da627cd3633ddbdefcd
malvinka <alina.popa@quantumblack.com>
2021-04-08 14:10:26 +0100

    experiment: tau 0.005 instead of 0.001, Result: > 700 episodes, worse than prev results

--- jamesbond.py
+++ jamesbond.py
@@ -10,8 +10,8 @@ import torch.optim as optim
 
 BUFFER_SIZE = int(1e6)  # replay buffer size
 BATCH_SIZE = 64         # minibatch size
-GAMMA = 0.999            # discount factor
-TAU = 1e-3              # for soft update of target parameters
+GAMMA = 0.99            # discount factor
+TAU = 5e-3              # for soft update of target parameters
 LR = 5e-4               # learning rate
 UPDATE_EVERY = 4        # how often to update the network
 
