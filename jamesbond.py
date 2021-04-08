610e5c4d660494738f2aabe345cfefb67070c52b
malvinka <alina.popa@quantumblack.com>
2021-04-08 14:22:49 +0100

    experiment: lr 0.0001 instead of 0.0005, Result: >500 episodes, worse than prev results

--- jamesbond.py
+++ jamesbond.py
@@ -11,8 +11,8 @@ import torch.optim as optim
 BUFFER_SIZE = int(1e6)  # replay buffer size
 BATCH_SIZE = 64         # minibatch size
 GAMMA = 0.99            # discount factor
-TAU = 5e-3              # for soft update of target parameters
-LR = 5e-4               # learning rate
+TAU = 1e-3              # for soft update of target parameters
+LR = 1e-4               # learning rate
 UPDATE_EVERY = 4        # how often to update the network
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
