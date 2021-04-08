c1edb26f26c5d35307ec40859f831fc19cce7e52
malvinka <alina.popa@quantumblack.com>
2021-04-08 14:00:58 +0100

    experiment: Gamma 0.999 instead of 0.99, Result: 559 episodes, worse than prev results

--- jamesbond.py
+++ jamesbond.py
@@ -10,7 +10,7 @@ import torch.optim as optim
 
 BUFFER_SIZE = int(1e6)  # replay buffer size
 BATCH_SIZE = 64         # minibatch size
-GAMMA = 0.9            # discount factor
+GAMMA = 0.999            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR = 5e-4               # learning rate
 UPDATE_EVERY = 4        # how often to update the network
